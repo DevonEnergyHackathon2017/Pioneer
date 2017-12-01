8eb3af128ef896507fb71f52420b287a30474e41
Fei Deng <fei.deng@pxd.com>
2017-12-01 00:14:25 -0600

    submit version

---
+++ ScanMeasure/ScanMeasure/ScanMeasure/TabbedPages.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScanMeasure
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPages : TabbedPage
+    {
+        public TabbedPages ()
+        {
+            InitializeComponent();
+        }
+
+        async void OnLogoutButtonClicked(object sender, EventArgs e)
+        {
+            App.IsUserLoggedIn = false;
+            Navigation.InsertPageBefore(new LoginPage(), this);
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ ScanMeasure/ScanMeasure/ScanMeasure/TallyPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScanMeasure
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TallyPage : ContentPage
+	{
+		public TallyPage ()
+		{
+			InitializeComponent ();
+            PhasePicker.SelectedIndex = 0;
+
+        }
+	}
+}
